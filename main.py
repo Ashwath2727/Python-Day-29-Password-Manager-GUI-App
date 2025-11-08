fbeaa8c2ba1c43ef164ca4e02855f178a4708b82
Ashwath S <miniash123@outlook.com>
2025-11-08 20:23:06 +0530

    added logo ui logic

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-
+import tkinter as tk
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = tk.Canvas(width=200, height=200)
+logo_img =tk.PhotoImage(file="logo.png")
+canvas.create_image(100, 113, image=logo_img)
+
+
+canvas.grid(row=0, column=0)
+
+
+window.mainloop()
